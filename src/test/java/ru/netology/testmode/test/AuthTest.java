aff2bcc56de67b1a8fb1a810844e1d9d9d92a3d8
ElenaLilu <elena.imyarekova1@gmail.com>
2023-10-10 09:57:59 +0300

    src

---
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -0,0 +1,90 @@
+package ru.netology.testmode.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
+import static ru.netology.testmode.data.DataGenerator.getRandomLogin;
+import static ru.netology.testmode.data.DataGenerator.getRandomPassword;
+
+class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        SelenideElement form = $(new By.ByTagName("form"));
+        form.$("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        form.$("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        form.$("[data-test-id=action-login]").click();
+
+        $(new By.ByTagName("h2")).shouldHave(Condition.partialText("Личный кабинет"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        SelenideElement form = $(new By.ByTagName("form"));
+        form.$("[data-test-id=login] input").setValue(notRegisteredUser.getLogin());
+        form.$("[data-test-id=password] input").setValue(notRegisteredUser.getPassword());
+        form.$("[data-test-id=action-login]").click();
+
+        assertNotificationMessage("Ошибка! Неверно указан логин или пароль");
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        SelenideElement form = $(new By.ByTagName("form"));
+        form.$("[data-test-id=login] input").setValue(blockedUser.getLogin());
+        form.$("[data-test-id=password] input").setValue(blockedUser.getPassword());
+        form.$("[data-test-id=action-login]").click();
+
+        assertNotificationMessage("Ошибка! Пользователь заблокирован");
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+        SelenideElement form = $(new By.ByTagName("form"));
+        form.$("[data-test-id=login] input").setValue(wrongLogin);
+        form.$("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        form.$("[data-test-id=action-login]").click();
+
+        assertNotificationMessage("Ошибка! Неверно указан логин или пароль");
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomPassword();
+        SelenideElement form = $(new By.ByTagName("form"));
+        form.$("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        form.$("[data-test-id=password] input").setValue(wrongPassword);
+        form.$("[data-test-id=action-login]").click();
+
+        assertNotificationMessage("Ошибка! Неверно указан логин или пароль");
+    }
+
+    private void assertNotificationMessage(String message) {
+        SelenideElement notification = $("[data-test-id=error-notification]");
+        notification.$("[class=notification__content]").shouldHave(Condition.exactText(message));
+    }
+}
